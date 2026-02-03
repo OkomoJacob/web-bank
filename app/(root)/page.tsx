5835f377c6381c60638361333ccc060f36d340eb
okomojacob <okomo.code@gmail.com>
2026-02-03 15:07:32 +0300

    :feat(dash): Initial Dahboard setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Jacob" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactiosn efficiently."
+          />
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.85}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
